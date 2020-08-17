b942517e8303f37eaddf4921d79859d982a2b1ba
Sureyya-Huseynova <suramail93@gmail.com>
2020-08-17 10:05:13 -0700

    quizexam

--- app.js
+++ app.js
@@ -1,61 +1,17 @@
-
-var ques = document.querySelector('.ques');
-var ques_num = document.querySelector('.ques_num');
+var text = document.querySelector('.text');
+var begin = document.querySelector('.begin');
+var quiz_div = document.querySelector('.quiz_div');
+var question_number = document.querySelector('.question_number');
+var question = document.querySelector('.question');
 var a = document.querySelector('.a');
 var b = document.querySelector('.b');
 var c = document.querySelector('.c');
 var d = document.querySelector('.d');
-var quiz_div = document.querySelector('.quiz_div');
-var begin = document.querySelector('.begin');
-var Bal = document.getElementById('bal');
-var text = document.querySelector('.text');
-
+var point_div = document.getElementById('point');
 var index = 1;
-var bal = 0;
+var point = 0;
 
-
-function App() {
-    fetch('https://opentdb.com/api.php?amount=11&category=18&difficulty=easy&type=multiple')
-        .then(res => res.json())
-        .then(datas => {
-            one = datas.results[index];
-            ques_num.innerHTML = index + " .";
-            ques.innerHTML = one.question;
-            var random = Math.random() * 10 + 1;
-            var texmin = Math.floor(random <= 4 ? random : random / 4);
-
-            if (a.id == texmin) {
-                a.innerHTML = one.correct_answer;
-                b.innerHTML = one.incorrect_answers[0];
-                c.innerHTML = one.incorrect_answers[1];
-                d.innerHTML = one.incorrect_answers[2];
-            }
-            if (b.id == texmin) {
-                b.innerHTML = one.correct_answer;
-                a.innerHTML = one.incorrect_answers[0];
-                c.innerHTML = one.incorrect_answers[1];
-                d.innerHTML = one.incorrect_answers[2];
-            }
-            if (c.id == texmin) {
-                c.innerHTML = one.correct_answer;
-                a.innerHTML = one.incorrect_answers[0];
-                b.innerHTML = one.incorrect_answers[1];
-                d.innerHTML = one.incorrect_answers[2];
-            }
-            if (d.id == texmin) {
-                d.innerHTML = one.correct_answer;
-                a.innerHTML = one.incorrect_answers[0];
-                b.innerHTML = one.incorrect_answers[1];
-                c.innerHTML = one.incorrect_answers[2];
-            }
-
-            var secimes = document.querySelectorAll('.secim');
-            secimes.forEach(secim => {
-                secim.style.backgroundColor = " rgb(255, 255, 236)";
-            });
-        })
-}
-
+// when you click begin button, you will see a question div
 begin.addEventListener('click', beginImtahan);
 function beginImtahan() {
     begin.style.display = "none";
@@ -64,52 +20,96 @@ function beginImtahan() {
     quiz_div.style.display = "block";
 };
 
-var secimes = document.querySelectorAll('.secim');
-secimes.forEach(secim => {
-    secim.addEventListener('click', Cehk);
-    function Cehk() {
-        if (secim.innerHTML == one.correct_answer) {
-            secim.style.backgroundColor = "rgb(55, 238, 55)";
-            bal += 10;
+// Take questions from the api and copy the correct answer in different variants each time
+function App() {
+    fetch('https://opentdb.com/api.php?amount=11&category=18&difficulty=easy&type=multiple')
+        .then(res => res.json())
+        .then(datas => {
+            one = datas.results[index];
+            question_number.innerHTML = index + " .";
+            question.innerHTML = one.question;
+            var random10 = Math.random() * 10 + 1;
+            var random4 = Math.floor(random10 <= 4 ? random10 : random10 / 4);
+
+            if (a.id == random4) {
+                a.innerHTML = one.correct_answer;
+                b.innerHTML = one.incorrect_answers[0];
+                c.innerHTML = one.incorrect_answers[1];
+                d.innerHTML = one.incorrect_answers[2];
+            }
+            if (b.id == random4) {
+                b.innerHTML = one.correct_answer;
+                a.innerHTML = one.incorrect_answers[0];
+                c.innerHTML = one.incorrect_answers[1];
+                d.innerHTML = one.incorrect_answers[2];
+            }
+            if (c.id == random4) {
+                c.innerHTML = one.correct_answer;
+                a.innerHTML = one.incorrect_answers[0];
+                b.innerHTML = one.incorrect_answers[1];
+                d.innerHTML = one.incorrect_answers[2];
+            }
+            if (d.id == random4) {
+                d.innerHTML = one.correct_answer;
+                a.innerHTML = one.incorrect_answers[0];
+                b.innerHTML = one.incorrect_answers[1];
+                c.innerHTML = one.incorrect_answers[2];
+            }
+
+            var options = document.querySelectorAll('.option');
+            options.forEach(option => {
+                option.style.backgroundColor = " rgb(255, 255, 236)";
+            });
+        })
+};
+
+// choose the answer
+var options = document.querySelectorAll('.option');
+options.forEach(option => {
+    option.addEventListener('click', CehkAnswer);
+    function CehkAnswer() {
+        if (option.innerHTML == one.correct_answer) {
+            option.style.backgroundColor = "rgb(55, 238, 55)";
+            point += 10;
         } else {
-            secim.style.backgroundColor = "red";
-            bal -= 2;
+            option.style.backgroundColor = "red";
+            point -= 2;
         }
         if (index < 10) {
             index++;
             App();
         } else {
-            D();
+            ResultModule();
         }
-
     }
-
 });
 
-
-function D() {
+// when the question or question time, you will see your result
+function ResultModule() {
     if (index = 4) {
-        var netice = document.querySelector('.netice');
-        var module = document.getElementById('module');
-        module.style.display = 'block';
+        var result = document.querySelector('.result');
+        var result_module = document.getElementById('result_module');
+        result_module.style.display = 'block';
         quiz_div.style.display = "none"
-        Bal.innerHTML = bal;
-        if (bal > 40) {
-            netice.innerHTML = '<p>Kecdiniz</p>'
+        console.log(point);
+        point_div.innerHTML = point;
+        if (point > 40) {
+            result.innerHTML = '<p>Kecdiniz</p>'
         } else {
-            netice.innerHTML = '<p>Kesildiniz</p>'
+            result.innerHTML = '<p>Kesildiniz</p>'
         }
     }
 };
 
+// click x on the result div . resultdiv is none.
+document.querySelector('.close').addEventListener('click',function(){
+    result_module.style.display="none";
+});
+
+// total time alloted to all question
 setTimeout(function () {
     quiz_div.style.display = 'none';
-
-    D();
+    ResultModule();
 }, 120000)
-
-document.querySelector('.close').addEventListener('click',function(){
-    module.style.display="none";
-})
 
 
