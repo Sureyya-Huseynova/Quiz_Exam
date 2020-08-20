fe7133607e030d08b5d7a5a618d0af787f7d0ff9
Sureyya-Huseynova <suramail93@gmail.com>
2020-08-20 07:59:09 -0700

    load ve time duzeldi

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var b = document.querySelector('.b');
 var c = document.querySelector('.c');
 var d = document.querySelector('.d');
 var point_div = document.getElementById('point');
+var loading = document.querySelector('.load');
+var question_div = document.querySelector('.question_div');
+var All_Options_dives = document.querySelector('.All_Options_dives');
 var index = 1;
 var point = 0;
 
@@ -18,56 +21,71 @@ function beginImtahan() {
     text.style.display = "none";
     App();
     quiz_div.style.display = "block";
+
+    // total time alloted to all question
+    setTimeout(function () {
+        quiz_div.style.display = 'none';
+        ResultModule();
+    }, 120000);
 };
 
 // Take questions from the api and copy the correct answer in different variants each time
 function App() {
+    setTimeout(() => {
+        question_div.style.display = "none";
+        All_Options_dives.style.display = "none";
+        loading.style.display = "block";
+    }, 200);
     fetch('https://opentdb.com/api.php?amount=11&category=18&difficulty=easy&type=multiple')
         .then(res => res.json())
         .then(datas => {
-            one = datas.results[index];
-            question_number.innerHTML = index + " .";
-            question.innerHTML = one.question;
-            var random10 = Math.random() * 10 + 1;
-            var random4 = Math.floor(random10 <= 4 ? random10 : random10 / 4);
-
-            if (a.id == random4) {
-                a.innerHTML = one.correct_answer;
-                b.innerHTML = one.incorrect_answers[0];
-                c.innerHTML = one.incorrect_answers[1];
-                d.innerHTML = one.incorrect_answers[2];
-            }
-            if (b.id == random4) {
-                b.innerHTML = one.correct_answer;
-                a.innerHTML = one.incorrect_answers[0];
-                c.innerHTML = one.incorrect_answers[1];
-                d.innerHTML = one.incorrect_answers[2];
-            }
-            if (c.id == random4) {
-                c.innerHTML = one.correct_answer;
-                a.innerHTML = one.incorrect_answers[0];
-                b.innerHTML = one.incorrect_answers[1];
-                d.innerHTML = one.incorrect_answers[2];
-            }
-            if (d.id == random4) {
-                d.innerHTML = one.correct_answer;
-                a.innerHTML = one.incorrect_answers[0];
-                b.innerHTML = one.incorrect_answers[1];
-                c.innerHTML = one.incorrect_answers[2];
-            }
-
-            var options = document.querySelectorAll('.option');
-            options.forEach(option => {
-                option.style.backgroundColor = " rgb(255, 255, 236)";
-            });
-        })
+           one = datas.results[index];       
+                question_number.innerHTML = index + " .";
+                question.innerHTML = one.question;
+                var random10 = Math.random() * 10 + 1;
+                var random4 = Math.floor(random10 <= 4 ? random10 : random10 / 4);
+                if (a.id == random4) {
+                    a.innerHTML = one.correct_answer;
+                    b.innerHTML = one.incorrect_answers[0];
+                    c.innerHTML = one.incorrect_answers[1];
+                    d.innerHTML = one.incorrect_answers[2];
+                }
+                if (b.id == random4) {
+                    b.innerHTML = one.correct_answer;
+                    a.innerHTML = one.incorrect_answers[0];
+                    c.innerHTML = one.incorrect_answers[1];
+                    d.innerHTML = one.incorrect_answers[2];
+                }
+                if (c.id == random4) {
+                    c.innerHTML = one.correct_answer;
+                    a.innerHTML = one.incorrect_answers[0];
+                    b.innerHTML = one.incorrect_answers[1];
+                    d.innerHTML = one.incorrect_answers[2];
+                }
+                if (d.id == random4) {
+                    d.innerHTML = one.correct_answer;
+                    a.innerHTML = one.incorrect_answers[0];
+                    b.innerHTML = one.incorrect_answers[1];
+                    c.innerHTML = one.incorrect_answers[2];
+                }
+                var options = document.querySelectorAll('.option');
+                options.forEach(option => {
+                    option.style.backgroundColor = " rgb(255, 255, 236)";
+                });
+                question_div.style.display = "block";
+                All_Options_dives.style.display = "block";
+                loading.style.display = "none"; 
+        });
 };
+
+  
+
 
 // choose the answer
 var options = document.querySelectorAll('.option');
 options.forEach(option => {
-    option.addEventListener('click', CehkAnswer);
-    function CehkAnswer() {
+    option.addEventListener('click', CehkcAnswer);
+    function CehkcAnswer() {
         if (option.innerHTML == one.correct_answer) {
             option.style.backgroundColor = "rgb(55, 238, 55)";
             point += 10;
@@ -75,14 +93,16 @@ options.forEach(option => {
             option.style.backgroundColor = "red";
             point -= 2;
         }
-        if (index < 10) {
+        if (index <= 9) {
             index++;
             App();
-        } else {
+        }
+        else {
             ResultModule();
         }
     }
 });
+
 
 // when the question or question time, you will see your result
 function ResultModule() {
@@ -102,14 +122,11 @@ function ResultModule() {
 };
 
 // click x on the result div . resultdiv is none.
-document.querySelector('.close').addEventListener('click',function(){
-    result_module.style.display="none";
+document.querySelector('.close').addEventListener('click', function () {
+    result_module.style.display = "none";
 });
 
-// total time alloted to all question
-setTimeout(function () {
-    quiz_div.style.display = 'none';
-    ResultModule();
-}, 120000)
+
+
 
 
